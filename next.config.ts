c0010a67acce3ffac097d38d233c7c6a169a3bf4
RAKSHIT Gupta <rakshit2004gupta@gmail.com>
2025-12-15 19:24:20 +0000

    https://imagefa.st/image/WhatsApp-Image-2025-12-16-at-12.27.30-AM.xzLli

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'imagefa.st',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
