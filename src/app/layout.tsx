6d9f676a164cfaed7dc36e2f9b47787dbc87c977
RAKSHIT Gupta <rakshit2004gupta@gmail.com>
2025-12-15 19:35:07 +0000

    still when i press in naviagation buttons the effect is not smooth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import { Toaster } from "@/components/ui/toaster"
 import './globals.css';
+import { cn } from '@/lib/utils';
 
 export const metadata: Metadata = {
   title: 'DataScribe Portfolio',
